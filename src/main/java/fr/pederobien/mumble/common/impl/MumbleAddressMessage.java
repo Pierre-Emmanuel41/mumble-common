9c56814711450e4c701ef85d6edc658079774597
Pierre-Emmanuel41 <pierre.emmanuel41@gmail.com>
2021-11-03 14:10:00 +0100

    communication: Refactoring Udp protocol

--- src/main/java/fr/pederobien/mumble/common/impl/MumbleAddressMessage.java
+++ src/main/java/fr/pederobien/mumble/common/impl/MumbleAddressMessage.java
@@ -8,14 +8,28 @@ import fr.pederobien.messenger.interfaces.IMessage;
 public class MumbleAddressMessage extends AddressMessage {
 	private IMessage<Header> message;
 
+	/**
+	 * Create a request message to be send to a remote.
+	 * 
+	 * @param message The message that contains the bytes to send to the remote and the identifier.
+	 * @param address The address at which the message should be sent.
+	 */
 	public MumbleAddressMessage(IMessage<Header> message, InetSocketAddress address) {
 		super(message.getBytes(), message.getIdentifier(), address);
 		this.message = message;
+	}
+
+	/**
+	 * Create a request message to be send to a remote.
+	 * 
+	 * @param message The message that contains the bytes to send to the remote and the identifier.
+	 */
+	public MumbleAddressMessage(IMessage<Header> message) {
+		this(message, null);
 	}
 
 	@Override
 	public String toString() {
 		return message.toString();
 	}
-
 }
