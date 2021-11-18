cc2483037ba12ea8d92f239411f2bc5b7dfa9604
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2021-11-18 10:59:56 +0100

    Upgrading ParameterType class for conversion

--- src/main/java/fr/pederobien/mumble/common/impl/ParameterType.java
+++ src/main/java/fr/pederobien/mumble/common/impl/ParameterType.java
@@ -124,6 +124,17 @@ public class ParameterType<T> {
 		return parser == null ? null : parser.getValue(bytes);
 	}
 
+	/**
+	 * Retrieve the value associated to the given string representation.
+	 * 
+	 * @param value the string representation of the value to retrieve.
+	 * 
+	 * @return The object value associated to the given string representation.
+	 */
+	public T getValue(String value) {
+		return parser == null ? null : parser.getValue(value);
+	}
+
 	@Override
 	public String toString() {
 		return clazz.getSimpleName();
@@ -133,54 +144,51 @@ public class ParameterType<T> {
 		/**
 		 * Parser that transform boolean to bytes array and retrieve the boolean value from the bytes array.
 		 */
-		public static final Parser<Boolean> BOOLEAN = new Parser<Boolean>(value -> value.getWrapper().putInt(value.getValue() ? 1 : 0).get(),
-				value -> value.getWrapper().getInt(0) == 1);
+		public static final Parser<Boolean> BOOLEAN = new Parser<Boolean>(ValueToBytesConverter.BOOLEAN, BytesToValueConverter.BOOLEAN, StringToValueConverter.BOOLEAN);
 
 		/**
 		 * Parser that transform character to bytes array and retrieve the character value from the bytes array.
 		 */
-		public static final Parser<Character> CHARACTER = new Parser<Character>(value -> value.getWrapper().put((byte) (char) value.getValue()).get(),
-				value -> (char) value.getWrapper().get(0));
+		public static final Parser<Character> CHARACTER = new Parser<Character>(ValueToBytesConverter.CHAR, BytesToValueConverter.CHAR, StringToValueConverter.CHAR);
 
 		/**
 		 * Parser that transform byte to bytes array and retrieve the byte value from the bytes array.
 		 */
-		public static final Parser<Byte> BYTE = new Parser<Byte>(value -> value.getWrapper().put(value.getValue()).get(), value -> value.getWrapper().get(0));
+		public static final Parser<Byte> BYTE = new Parser<Byte>(ValueToBytesConverter.BYTE, BytesToValueConverter.BYTE, StringToValueConverter.BYTE);
 
 		/**
 		 * Parser that transform short to bytes array and retrieve the short value from the bytes array.
 		 */
-		public static final Parser<Short> SHORT = new Parser<Short>(value -> value.getWrapper().putShort(value.getValue()).get(),
-				value -> value.getWrapper().getShort(0));
+		public static final Parser<Short> SHORT = new Parser<Short>(ValueToBytesConverter.SHORT, BytesToValueConverter.SHORT, StringToValueConverter.SHORT);
 
 		/**
 		 * Parser that transform integer to bytes array and retrieve the integer value from the bytes array.
 		 */
-		public static final Parser<Integer> INT = new Parser<Integer>(value -> value.getWrapper().putInt(value.getValue()).get(), value -> value.getWrapper().getInt(0));
+		public static final Parser<Integer> INT = new Parser<Integer>(ValueToBytesConverter.INT, BytesToValueConverter.INT, StringToValueConverter.INT);
 
 		/**
 		 * Parser that transform long to bytes array and retrieve the long value from the bytes array.
 		 */
-		public static final Parser<Long> LONG = new Parser<Long>(value -> value.getWrapper().putLong(value.getValue()).get(), value -> value.getWrapper().getLong(0));
+		public static final Parser<Long> LONG = new Parser<Long>(ValueToBytesConverter.LONG, BytesToValueConverter.LONG, StringToValueConverter.LONG);
 
 		/**
 		 * Parser that transform float to bytes array and retrieve the float value from the bytes array.
 		 */
-		public static final Parser<Float> FLOAT = new Parser<Float>(value -> value.getWrapper().putFloat(value.getValue()).get(),
-				value -> value.getWrapper().getFloat(0));
+		public static final Parser<Float> FLOAT = new Parser<Float>(ValueToBytesConverter.FLOAT, BytesToValueConverter.FLOAT, StringToValueConverter.FLOAT);
 
 		/**
 		 * Parser that transform double to bytes array and retrieve the double value from the bytes array.
 		 */
-		public static final Parser<Double> DOUBLE = new Parser<Double>(value -> value.getWrapper().putDouble(value.getValue()).get(),
-				value -> value.getWrapper().getDouble(0));
+		public static final Parser<Double> DOUBLE = new Parser<Double>(ValueToBytesConverter.DOUBLE, BytesToValueConverter.DOUBLE, StringToValueConverter.DOUBLE);
 
-		private Function<WrappedValue<U>, byte[]> bytesGenerator;
-		private Function<WrappedValue<U>, U> valueGenerator;
+		private ValueToBytesConverter<U> bytesConverter;
+		private BytesToValueConverter<U> valueConverter;
+		private StringToValueConverter<U> stringConverter;
 
-		public Parser(Function<WrappedValue<U>, byte[]> bytesGenerator, Function<WrappedValue<U>, U> valueGenerator) {
-			this.bytesGenerator = bytesGenerator;
-			this.valueGenerator = valueGenerator;
+		public Parser(ValueToBytesConverter<U> bytesConverter, BytesToValueConverter<U> valueConverter, StringToValueConverter<U> stringConverter) {
+			this.bytesConverter = bytesConverter;
+			this.valueConverter = valueConverter;
+			this.stringConverter = stringConverter;
 		}
 
 		/**
@@ -191,7 +199,7 @@ public class ParameterType<T> {
 		 * @return The bytes array corresponding to the specified value.
 		 */
 		public byte[] getBytes(U value) {
-			return bytesGenerator.apply(new WrappedValue<U>(value));
+			return bytesConverter.apply(value);
 		}
 
 		/**
@@ -202,7 +210,11 @@ public class ParameterType<T> {
 		 * @return The object value associated to the given bytes representation.
 		 */
 		public U getValue(byte[] bytes) {
-			return valueGenerator.apply(new WrappedValue<U>(bytes));
+			return valueConverter.apply(bytes);
+		}
+
+		public U getValue(String value) {
+			return stringConverter.apply(value);
 		}
 	}
 
@@ -231,6 +243,164 @@ public class ParameterType<T> {
 		 */
 		public ByteWrapper getWrapper() {
 			return wrapper;
+		}
+	}
+
+	private static class ValueToBytesConverter<U> {
+		/**
+		 * Converter that converts a boolean value into a bytes array.
+		 */
+		public static final ValueToBytesConverter<Boolean> BOOLEAN = new ValueToBytesConverter<Boolean>(
+				value -> value.getWrapper().putInt(value.getValue() ? 1 : 0).get());
+
+		/**
+		 * Converter that converts a character value into a bytes array.
+		 */
+		public static final ValueToBytesConverter<Character> CHAR = new ValueToBytesConverter<Character>(
+				value -> value.getWrapper().put((byte) (char) value.getValue()).get());
+
+		/**
+		 * Converter that converts a byte value into a bytes array.
+		 */
+		public static final ValueToBytesConverter<Byte> BYTE = new ValueToBytesConverter<Byte>(value -> value.getWrapper().put(value.getValue()).get());
+
+		/**
+		 * Converter that converts a short value into a bytes array.
+		 */
+		public static final ValueToBytesConverter<Short> SHORT = new ValueToBytesConverter<Short>(value -> value.getWrapper().putShort(value.getValue()).get());
+
+		/**
+		 * Converter that converts a integer value into a bytes array.
+		 */
+		public static final ValueToBytesConverter<Integer> INT = new ValueToBytesConverter<Integer>(value -> value.getWrapper().putInt(value.getValue()).get());
+
+		/**
+		 * Converter that converts a long value into a bytes array.
+		 */
+		public static final ValueToBytesConverter<Long> LONG = new ValueToBytesConverter<Long>(value -> value.getWrapper().putLong(value.getValue()).get());
+
+		/**
+		 * Converter that converts a float value into a bytes array.
+		 */
+		public static final ValueToBytesConverter<Float> FLOAT = new ValueToBytesConverter<Float>(value -> value.getWrapper().putFloat(value.getValue()).get());
+
+		/**
+		 * Converter that converts a double value into a bytes array.
+		 */
+		public static final ValueToBytesConverter<Double> DOUBLE = new ValueToBytesConverter<Double>(value -> value.getWrapper().putDouble(value.getValue()).get());
+
+		private Function<WrappedValue<U>, byte[]> converter;
+
+		public ValueToBytesConverter(Function<WrappedValue<U>, byte[]> converter) {
+			this.converter = converter;
+		}
+
+		public byte[] apply(U value) {
+			return converter.apply(new WrappedValue<U>(value));
+		}
+	}
+
+	private static class BytesToValueConverter<U> {
+		/**
+		 * Converter that converts a bytes array value into an boolean value.
+		 */
+		public static final BytesToValueConverter<Boolean> BOOLEAN = new BytesToValueConverter<Boolean>(value -> value.getWrapper().getInt(0) == 1);
+
+		/**
+		 * Converter that converts a bytes array value into an character value.
+		 */
+		public static final BytesToValueConverter<Character> CHAR = new BytesToValueConverter<Character>(value -> (char) value.getWrapper().get(0));
+
+		/**
+		 * Converter that converts a bytes array value into an byte value.
+		 */
+		public static final BytesToValueConverter<Byte> BYTE = new BytesToValueConverter<Byte>(value -> value.getWrapper().get(0));
+
+		/**
+		 * Converter that converts a bytes array value into an short value.
+		 */
+		public static final BytesToValueConverter<Short> SHORT = new BytesToValueConverter<Short>(value -> value.getWrapper().getShort(0));
+
+		/**
+		 * Converter that converts a bytes array value into an integer value.
+		 */
+		public static final BytesToValueConverter<Integer> INT = new BytesToValueConverter<Integer>(value -> value.getWrapper().getInt(0));
+
+		/**
+		 * Converter that converts a bytes array value into an long value.
+		 */
+		public static final BytesToValueConverter<Long> LONG = new BytesToValueConverter<Long>(value -> value.getWrapper().getLong(0));
+
+		/**
+		 * Converter that converts a bytes array value into an float value.
+		 */
+		public static final BytesToValueConverter<Float> FLOAT = new BytesToValueConverter<Float>(value -> value.getWrapper().getFloat(0));
+
+		/**
+		 * Converter that converts a bytes array value into an double value.
+		 */
+		public static final BytesToValueConverter<Double> DOUBLE = new BytesToValueConverter<Double>(value -> value.getWrapper().getDouble(0));
+
+		private Function<WrappedValue<U>, U> converter;
+
+		public BytesToValueConverter(Function<WrappedValue<U>, U> converter) {
+			this.converter = converter;
+		}
+
+		public U apply(byte[] value) {
+			return converter.apply(new WrappedValue<U>(value));
+		}
+	}
+
+	private static class StringToValueConverter<U> {
+		/**
+		 * Converter that converts a boolean value into a bytes array.
+		 */
+		public static final StringToValueConverter<Boolean> BOOLEAN = new StringToValueConverter<Boolean>(value -> Boolean.parseBoolean(value));
+
+		/**
+		 * Converter that converts a character value into a bytes array.
+		 */
+		public static final StringToValueConverter<Character> CHAR = new StringToValueConverter<Character>(value -> value.charAt(0));
+
+		/**
+		 * Converter that converts a byte value into a bytes array.
+		 */
+		public static final StringToValueConverter<Byte> BYTE = new StringToValueConverter<Byte>(value -> (byte) value.charAt(0));
+
+		/**
+		 * Converter that converts a short value into a bytes array.
+		 */
+		public static final StringToValueConverter<Short> SHORT = new StringToValueConverter<Short>(value -> Short.parseShort(value));
+
+		/**
+		 * Converter that converts a integer value into a bytes array.
+		 */
+		public static final StringToValueConverter<Integer> INT = new StringToValueConverter<Integer>(value -> Integer.parseInt(value));
+
+		/**
+		 * Converter that converts a long value into a bytes array.
+		 */
+		public static final StringToValueConverter<Long> LONG = new StringToValueConverter<Long>(value -> Long.parseLong(value));
+
+		/**
+		 * Converter that converts a float value into a bytes array.
+		 */
+		public static final StringToValueConverter<Float> FLOAT = new StringToValueConverter<Float>(value -> Float.parseFloat(value));
+
+		/**
+		 * Converter that converts a double value into a bytes array.
+		 */
+		public static final StringToValueConverter<Double> DOUBLE = new StringToValueConverter<Double>(value -> Double.parseDouble(value));
+
+		private Function<String, U> converter;
+
+		public StringToValueConverter(Function<String, U> converter) {
+			this.converter = converter;
+		}
+
+		public U apply(String value) {
+			return converter.apply(value);
 		}
 	}
 }
