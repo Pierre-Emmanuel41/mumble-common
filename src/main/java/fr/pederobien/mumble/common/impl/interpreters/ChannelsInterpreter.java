e179298efee9a6347b81365993aa582c126dcf20
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2021-11-19 14:22:42 +0100

    Adding sound modifier parameters in requests associated to Idc CHANNELS

--- src/main/java/fr/pederobien/mumble/common/impl/interpreters/ChannelsInterpreter.java
+++ src/main/java/fr/pederobien/mumble/common/impl/interpreters/ChannelsInterpreter.java
@@ -4,17 +4,18 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import fr.pederobien.mumble.common.impl.ParameterType;
 import fr.pederobien.utils.ByteWrapper;
 
 public class ChannelsInterpreter extends AbstractInterpreter {
 
 	@Override
 	protected byte[] internalGenerate(Object[] payload) {
+		int currentIndex = 0;
+		ByteWrapper wrapper = ByteWrapper.create();
+
 		switch (getHeader().getOid()) {
 		case GET:
-			int currentIndex = 0;
-			ByteWrapper wrapper = ByteWrapper.create();
-
 			// Number of channels
 			int numberOfChannels = (int) payload[currentIndex++];
 			wrapper.putInt(numberOfChannels);
@@ -23,8 +24,24 @@ public class ChannelsInterpreter extends AbstractInterpreter {
 				// Channel's name
 				wrapper.putString((String) payload[currentIndex++], true);
 
-				// Channel's sound modifier name
+				// Modifier's name
 				wrapper.putString((String) payload[currentIndex++], true);
+
+				// Number of parameter
+				int numberOfParameters = (int) payload[currentIndex++];
+				wrapper.putInt(numberOfParameters);
+
+				for (int j = 0; j < numberOfParameters; j++) {
+					// Parameter's name
+					wrapper.putString((String) payload[currentIndex++], true);
+
+					// Parameter's type
+					ParameterType<?> type = (ParameterType<?>) payload[currentIndex++];
+					wrapper.putInt(type.getCode());
+
+					// Parameter's value
+					wrapper.put(type.getBytes(payload[currentIndex++]));
+				}
 
 				// Number of players
 				int numberOfPlayers = (int) payload[currentIndex++];
@@ -43,7 +60,29 @@ public class ChannelsInterpreter extends AbstractInterpreter {
 			}
 			return wrapper.get();
 		case ADD:
-			return ByteWrapper.create().putString((String) payload[0], true).putString((String) payload[1], true).get();
+			// Channel' name
+			wrapper.putString((String) payload[currentIndex++], true);
+
+			// Modifier's name
+			wrapper.putString((String) payload[currentIndex++], true);
+
+			// Number of parameter
+			int numberOfParameters = (int) payload[currentIndex++];
+			wrapper.putInt(numberOfParameters);
+
+			for (int j = 0; j < numberOfParameters; j++) {
+				// Parameter's name
+				wrapper.putString((String) payload[currentIndex++], true);
+
+				// Parameter's type
+				ParameterType<?> type = (ParameterType<?>) payload[currentIndex++];
+				wrapper.putInt(type.getCode());
+
+				// Parameter's value
+				wrapper.put(type.getBytes(payload[currentIndex++]));
+			}
+
+			return wrapper.get();
 		case REMOVE:
 			return ByteWrapper.create().putString((String) payload[0]).get();
 		case SET:
@@ -79,6 +118,29 @@ public class ChannelsInterpreter extends AbstractInterpreter {
 				informations.add(wrapper.getString(first, soundModifierNameLength));
 				first += soundModifierNameLength;
 
+				// Number of parameters
+				int numberOfParameters = wrapper.getInt(first);
+				first += 4;
+				informations.add(numberOfParameters);
+
+				for (int j = 0; j < numberOfParameters; j++) {
+					// Parameter's name
+					int parameterNameLength = wrapper.getInt(first);
+					first += 4;
+					informations.add(wrapper.getString(first, parameterNameLength));
+					first += parameterNameLength;
+
+					// Parameter's type
+					int code = wrapper.getInt(first);
+					first += 4;
+					ParameterType<?> type = ParameterType.fromCode(code);
+					informations.add(type);
+
+					// Parameter's value
+					informations.add(type.getValue(wrapper.extract(first, type.size())));
+					first += type.size();
+				}
+
 				// Number of players
 				int numberOfPlayers = wrapper.getInt(first);
 				informations.add(numberOfPlayers);
@@ -101,7 +163,6 @@ public class ChannelsInterpreter extends AbstractInterpreter {
 				}
 			}
 			return informations.toArray();
-
 		case ADD:
 			// Channel's name
 			int channelNameLength = wrapper.getInt(first);
@@ -114,6 +175,29 @@ public class ChannelsInterpreter extends AbstractInterpreter {
 			first += 4;
 			informations.add(wrapper.getString(first, soundModifierNameLength));
 			first += soundModifierNameLength;
+
+			// Number of parameters
+			int numberOfParameters = wrapper.getInt(first);
+			first += 4;
+			informations.add(numberOfParameters);
+
+			for (int j = 0; j < numberOfParameters; j++) {
+				// Parameter's name
+				int parameterNameLength = wrapper.getInt(first);
+				first += 4;
+				informations.add(wrapper.getString(first, parameterNameLength));
+				first += parameterNameLength;
+
+				// Parameter's type
+				int code = wrapper.getInt(first);
+				first += 4;
+				ParameterType<?> type = ParameterType.fromCode(code);
+				informations.add(type);
+
+				// Parameter's value
+				informations.add(type.getValue(wrapper.extract(first, type.size())));
+				first += type.size();
+			}
 			return informations.toArray();
 		case REMOVE:
 			return Arrays.asList(wrapper.getString()).toArray();
