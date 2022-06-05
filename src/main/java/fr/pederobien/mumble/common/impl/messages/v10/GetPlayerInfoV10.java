23e90902471782632ac20b00021f91630ac07cbc
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2022-06-05 16:40:18 +0200

    Fix: Bad parameters order for request GET_PLAYER_INFO

--- src/main/java/fr/pederobien/mumble/common/impl/messages/v10/GetPlayerInfoV10.java
+++ src/main/java/fr/pederobien/mumble/common/impl/messages/v10/GetPlayerInfoV10.java
@@ -25,97 +25,93 @@ public class GetPlayerInfoV10 extends MumbleMessage {
 
 	@Override
 	public IMessage parse(byte[] payload) {
-		if (getHeader().isError())
+		if (payload.length == 0 || getHeader().isError())
 			return this;
 
 		List<Object> properties = new ArrayList<Object>();
 		int first = 0;
 		ByteWrapper wrapper = ByteWrapper.wrap(payload);
 
-		// Player name
-		int playerNameLength = wrapper.getInt(first);
+		// Player's online status
+		boolean isOnline = wrapper.getInt(first) == 1 ? true : false;
+		properties.add(isOnline);
 		first += 4;
-		String playerName = wrapper.getString(first, playerNameLength);
-		properties.add(playerName);
-		first += playerNameLength;
 
+		String name = null, gameAddress = null;
 		UUID identifier = null;
-		String gameAddress = null;
 		int gamePort = 0;
-		boolean isOnline = false, isAdmin = false, isMute = false, isDeafen = false;
+		boolean isAdmin = false, isMute = false, isDeafen = false;
 		double x = 0, y = 0, z = 0, yaw = 0, pitch = 0;
 
-		// When it is an answer
-		if (first < payload.length) {
+		if (isOnline) {
+			// Player name
+			int playerNameLength = wrapper.getInt(first);
+			first += 4;
+			name = wrapper.getString(first, playerNameLength);
+			properties.add(name);
+			first += playerNameLength;
 
-			// Player connected
-			isOnline = wrapper.getInt(first) == 1;
-			properties.add(isOnline);
+			// Player's identifier
+			int identifierLength = wrapper.getInt(first);
+			first += 4;
+			identifier = UUID.fromString(wrapper.getString(first, identifierLength));
+			properties.add(identifier);
+			first += identifierLength;
+
+			// Player's game address
+			int addressLength = wrapper.getInt(first);
+			first += 4;
+			gameAddress = wrapper.getString(first, addressLength);
+			properties.add(gameAddress);
+			first += addressLength;
+
+			// Player's game port
+			gamePort = wrapper.getInt(first);
+			properties.add(gamePort);
 			first += 4;
 
-			if (isOnline) {
-				// Player's identifier
-				int identifierLength = wrapper.getInt(first);
-				first += 4;
-				identifier = UUID.fromString(wrapper.getString(first, identifierLength));
-				properties.add(identifier);
-				first += identifierLength;
+			// Player's administrator status
+			isAdmin = wrapper.getInt(first) == 1 ? true : false;
+			properties.add(isAdmin);
+			first += 4;
 
-				// Player's game address
-				int addressLength = wrapper.getInt(first);
-				first += 4;
-				gameAddress = wrapper.getString(first, addressLength);
-				properties.add(gameAddress);
-				first += addressLength;
+			// Player's mute status
+			isMute = wrapper.getInt(first) == 1 ? true : false;
+			properties.add(isMute);
+			first += 4;
 
-				// Player's game port
-				gamePort = wrapper.getInt(first);
-				properties.add(gamePort);
-				first += 4;
+			// Player's deafen status
+			isDeafen = wrapper.getInt(first) == 1 ? true : false;
+			properties.add(isDeafen);
+			first += 4;
 
-				// Player's administrator status
-				isAdmin = wrapper.getInt(first) == 1 ? true : false;
-				properties.add(isAdmin);
-				first += 4;
+			// Player's x coordinate
+			x = wrapper.getDouble(first);
+			properties.add(x);
+			first += 8;
 
-				// Player's mute status
-				isMute = wrapper.getInt(first) == 1 ? true : false;
-				properties.add(isMute);
-				first += 4;
+			// Player's y coordinate
+			y = wrapper.getDouble(first);
+			properties.add(y);
+			first += 8;
 
-				// Player's deafen status
-				isDeafen = wrapper.getInt(first) == 1 ? true : false;
-				properties.add(isDeafen);
-				first += 4;
+			// Player's z coordinate
+			z = wrapper.getDouble(first);
+			properties.add(z);
+			first += 8;
 
-				// Player's x coordinate
-				x = wrapper.getDouble(first);
-				properties.add(x);
-				first += 8;
+			// Player's yaw angle
+			yaw = wrapper.getDouble(first);
+			properties.add(yaw);
+			first += 8;
 
-				// Player's y coordinate
-				y = wrapper.getDouble(first);
-				properties.add(y);
-				first += 8;
-
-				// Player's z coordinate
-				z = wrapper.getDouble(first);
-				properties.add(z);
-				first += 8;
-
-				// Player's yaw angle
-				yaw = wrapper.getDouble(first);
-				properties.add(yaw);
-				first += 8;
-
-				// Player's pitch angle
-				pitch = wrapper.getDouble(first);
-				properties.add(pitch);
-				first += 8;
-			}
+			// Player's pitch angle
+			pitch = wrapper.getDouble(first);
+			properties.add(pitch);
+			first += 8;
 		}
 
-		playerInfo = new FullPlayerInfo(playerName, isOnline, identifier, gameAddress, gamePort, isAdmin, isMute, isDeafen, x, y, z, yaw, pitch);
+		playerInfo = new FullPlayerInfo(name, isOnline, identifier, gameAddress, gamePort, isAdmin, isMute, isDeafen, x, y, z, yaw, pitch);
 		super.setProperties(properties.toArray());
 		return this;
 	}
@@ -124,60 +120,59 @@ public class GetPlayerInfoV10 extends MumbleMessage {
 	public void setProperties(Object... properties) {
 		super.setProperties(properties);
 
-		if (getHeader().isError())
+		if (properties.length == 0 || getHeader().isError())
 			return;
 
-		// Player's name
-		String playerName = (String) properties[0];
+		int currentIndex = 0;
 
+		// Player's online status
+		boolean isOnline = (boolean) properties[currentIndex++];
+
+		String name = null, gameAddress = null;
 		UUID identifier = null;
-		String gameAddress = null;
 		int gamePort = 0;
-		boolean isOnline = false, isAdmin = false, isMute = false, isDeafen = false;
+		boolean isAdmin = false, isMute = false, isDeafen = false;
 		double x = 0, y = 0, z = 0, yaw = 0, pitch = 0;
 
-		// When it is an answer
-		if (properties.length > 1) {
-			// Player's online status
-			isOnline = (boolean) properties[1];
+		if (isOnline) {
+			// Player name
+			name = (String) properties[currentIndex++];
 
-			if (isOnline) {
-				// Player's identifier
-				identifier = (UUID) properties[2];
+			// Player's identifier
+			identifier = (UUID) properties[currentIndex++];
 
-				// Player's game address
-				gameAddress = (String) properties[3];
+			// Player's game address
+			gameAddress = (String) properties[currentIndex++];
 
-				// Player's game port
-				gamePort = (int) properties[4];
+			// Player's game port
+			gamePort = (int) properties[currentIndex++];
 
-				// Player's administrator status
-				isAdmin = (boolean) properties[5];
+			// Player's administrator status
+			isAdmin = (boolean) properties[currentIndex++];
 
-				// Player's mute status
-				isMute = (boolean) properties[6];
+			// Player's mute status
+			isMute = (boolean) properties[currentIndex++];
 
-				// Player's deafen status
-				isDeafen = (boolean) properties[7];
+			// Player's deafen status
+			isDeafen = (boolean) properties[currentIndex++];
 
-				// Player's x coordinate
-				x = (double) properties[8];
+			// Player's x coordinate
+			x = (double) properties[currentIndex++];
 
-				// Player's y coordinate
-				y = (double) properties[9];
+			// Player's y coordinate
+			y = (double) properties[currentIndex++];
 
-				// Player's z coordinate
-				z = (double) properties[10];
+			// Player's z coordinate
+			z = (double) properties[currentIndex++];
 
-				// Player's yaw angle
-				yaw = (double) properties[11];
+			// Player's yaw angle
+			yaw = (double) properties[currentIndex++];
 
-				// Player's pitch angle
-				pitch = (double) properties[12];
-			}
+			// Player's pitch angle
+			pitch = (double) properties[currentIndex++];
 		}
 
-		playerInfo = new FullPlayerInfo(playerName, isOnline, identifier, gameAddress, gamePort, isAdmin, isMute, isDeafen, x, y, z, yaw, pitch);
+		playerInfo = new FullPlayerInfo(name, isOnline, identifier, gameAddress, gamePort, isAdmin, isMute, isDeafen, x, y, z, yaw, pitch);
 	}
 
 	@Override
@@ -187,49 +182,45 @@ public class GetPlayerInfoV10 extends MumbleMessage {
 		if (getHeader().isError())
 			return wrapper.get();
 
-		// Player's name
-		wrapper.putString(playerInfo.getName(), true);
+		// Player's online status
+		wrapper.putInt(playerInfo.isOnline() ? 1 : 0);
 
-		if (getProperties().length > 1) {
+		if (playerInfo.isOnline()) {
+			// Player's name
+			wrapper.putString(playerInfo.getName(), true);
 
-			// Player's online status
-			wrapper.putInt(playerInfo.isOnline() ? 1 : 0);
+			// Player's identifier
+			wrapper.putString(playerInfo.getIdentifier().toString(), true);
 
-			// Player connected
-			if (playerInfo.isOnline()) {
-				// Player's identifier
-				wrapper.putString(playerInfo.getIdentifier().toString(), true);
+			// Player's game address
+			wrapper.putString(playerInfo.getGameAddress().getAddress().getHostAddress(), true);
 
-				// Player's game address
-				wrapper.putString(playerInfo.getGameAddress().getAddress().getHostAddress(), true);
+			// Player's game port
+			wrapper.putInt(playerInfo.getGameAddress().getPort());
 
-				// Player's game port
-				wrapper.putInt(playerInfo.getGameAddress().getPort());
+			// Player's administrator status
+			wrapper.putInt(playerInfo.isAdmin() ? 1 : 0);
 
-				// Player's administrator status
-				wrapper.putInt(playerInfo.isAdmin() ? 1 : 0);
+			// Player's mute status
+			wrapper.putInt(playerInfo.isMute() ? 1 : 0);
 
-				// Player's mute status
-				wrapper.putInt(playerInfo.isMute() ? 1 : 0);
+			// Player's deafen status
+			wrapper.putInt(playerInfo.isDeafen() ? 1 : 0);
 
-				// Player's deafen status
-				wrapper.putInt(playerInfo.isDeafen() ? 1 : 0);
+			// Player's x coordinate
+			wrapper.putDouble(playerInfo.getX());
 
-				// Player's x coordinate
-				wrapper.putDouble(playerInfo.getX());
+			// Player's y coordinate
+			wrapper.putDouble(playerInfo.getY());
 
-				// Player's y coordinate
-				wrapper.putDouble(playerInfo.getY());
+			// Player's z coordinate
+			wrapper.putDouble(playerInfo.getZ());
 
-				// Player's z coordinate
-				wrapper.putDouble(playerInfo.getZ());
+			// Player's yaw angle
+			wrapper.putDouble(playerInfo.getYaw());
 
-				// Player's yaw angle
-				wrapper.putDouble(playerInfo.getYaw());
-
-				// Player's pitch angle
-				wrapper.putDouble(playerInfo.getPitch());
-			}
+			// Player's pitch
+			wrapper.putDouble(playerInfo.getPitch());
 		}
 
 		return wrapper.get();
